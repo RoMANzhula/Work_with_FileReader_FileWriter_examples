7b6c2e81763bb63dbea21161f83600f8ce32585b
RoMANzhula <roman1985al@ukr.net>
2023-04-21 15:16:26 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.io.*;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         String fileName1 = "C:\\Users\\Admin\\Desktop\\Новий текстовий документ.txt";
         String fileName2 = "C:\\Users\\Admin\\Desktop\\Новий текстовий документ (2).txt";
 
         writeOnlyWordsWithDigitsToAnotherFiles(fileName1, fileName2); //вызываем метод, который записывает из первого файла
         //слова, которые содержат в себе цифры, во второй файл
-
+        writeOnlyWordsWithLengthMoreThan(fileName1, fileName2, 7);//вызываем метод, который будет считывать символьные данные
+        //из одного файла и будет записывать слова, которые имеют количество символов больше, чем мы установим, в другой файл.
     }
 
     public static void writeOnlyWordsWithDigitsToAnotherFiles(String fileNameFrom, String fileNameTo) throws IOException { //метод,
@@ -36,6 +37,35 @@ public class Main {
         fileReader.close(); //очищаем ресурсы, закрываем поток чтения символов
         bufferedReader.close(); //очищаем ресурсы, закрываем буферизированный поток
         fileWriter.close(); //очищаем ресурсы, закрываем поток записи символов
+    }
+
+
+    public static void writeOnlyWordsWithLengthMoreThan(String fileNameFrom, String fileNameTo, int lengthOfWords) throws IOException { //метод
+        //который будет считывать символьные данные из одного файла и будет записывать слова, которые имеют количество символов
+        //больше, чем мы установим, в другой файл.
+        FileReader fileReader = new FileReader(fileNameFrom); //открываем поток для считывания символьных данных из указанного файла
+        BufferedReader bufferedReader = new BufferedReader(fileReader); //буферизируем входной поток (для оптимизации работы системы)
+        FileWriter fileWriter = new FileWriter(fileNameTo); //открываем поток для записи символьных данных в указанный файл
+
+
+        String finalWords = ""; //создаем строковую переменную, в которую будем складывать считанные слова (можно использовать StringBuilder,
+        //но для более понятного примера воспользуемся строкой)
+        while (bufferedReader.ready()) { //цикл будет выполняться, пока во входящем потоке будут символы для чтения
+            String line = bufferedReader.readLine(); //создаем строковую переменную, в которую будем читать данные целыми строками
+            String[] wordFromLine = line.split(" "); //считанную строку разбиваем на массив из слов с помощью разделителя пробел - " "
+            for (int i = 0; i < wordFromLine.length; i++) { //проходим по каждому слову в массиве
+                if (wordFromLine[i].length() > lengthOfWords) { //если длина слова(количество символов) будет превышать указанный нами размер, то
+                    finalWords += (wordFromLine[i] + ","); //добавляем это слово в нашу строку и следом ставим запятую
+                }
+            }
+        }
+        fileWriter.write(finalWords.substring(0, finalWords.length() - 1)); //с помощью исходящего(записывающего) потока пишем
+        //все слова через запятую в указанный файл и отбрасываем последнюю запятую
+
+        fileReader.close(); //очищаем ресурсы, закрываем поток чтения символов
+        bufferedReader.close(); //очищаем ресурсы, закрываем буферизированный поток
+        fileWriter.close(); //очищаем ресурсы, закрываем поток записи символов
+
     }
 
 }
